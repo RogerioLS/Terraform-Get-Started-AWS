727dd41d9a07297a0b4a55b118ca869f86e32eaa
Merovingio <60794624+RogerioLS@users.noreply.github.com>
2024-10-03 20:27:25 +0000

    terraform

--- learn-terraform-docker-container/main.tf
+++ learn-terraform-docker-container/main.tf
@@ -13,10 +13,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
